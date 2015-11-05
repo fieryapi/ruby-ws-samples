a6d8d191b0ad09287b7ab950d4aeb3cedb026d53
Hovik Manvelyan <hovik.manvelyan@efi.com>
2015-11-05 10:44:40 -0800

    Initial commit

--- README.md
+++ README.md
@@ -1,4 +1,12 @@
-ruby-fiery-events-sample
-===============
+## Sample Ruby Program To Receive Fiery Events
 
-sample ruby program to demonstrate how to receive fiery events
+Setup the bundler in `src` directory with `bundle install`, modify `fiery_events.rb` in the configuration section to connect to the Fiery then execute the script with `bundle exec ruby fiery_events.rb`
+
+The sample program runs three scenarios:
+  * Receive only Fiery status change events
+  * Receive only job is printing? events
+  * Receive only job is printing? events in batch mode
+ 
+If you want to move to the next scenario, simply press `enter` key
+
+**Note** Always use secure connection (HTTPS) when connecting to Fiery API in production.
---
+++ src/fiery_events.rb
@@ -0,0 +1,219 @@
+require 'rubygems'
+
+# Set up gems listed in the Gemfile.
+ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __FILE__)
+require 'bundler/setup' if File.exists?(ENV['BUNDLE_GEMFILE'])
+
+
+require 'eventmachine'
+require 'faye/websocket'
+require 'json'
+require 'rest-client'
+
+
+## *****************************************************************************
+## configuration section
+## *****************************************************************************
+
+# set the host name as fiery server name or ip address
+$hostname = '10.3.51.160'
+
+# set the key to access Fiery API
+$api_key = 'hscPvGULGj8dNgLUVGdObw2+TuFt6gEmDYNQ92mGPASxlInlXXVHiyDrnNCJg239+W5K6ow++Yr9tn3p+sJdCKEtDO8/rrT26QWrUXmzbswH1DmwrADnpMdXCSw0ZNBrrmHAtfUN0ARDd8trWCmk+9QvrLSfeqTPqGmyrdCk2idtZEl1C/o6B5mRQzeUNqUVdI0vBOVaPULW0QW1tT+MQN6JMDgXZRJm8lC4+HqXAr13vo8pQy/TbY9XaNCyZOMyUqxGftSWGHRzsZa3RL5lFMGtUufkqPpaP7w22dFYmiYkXI0vtYn7VsUr+5r/y7M0MVnw5GiqDwAJ0ce9QIbjHQ=='
+
+# set the username to login to the fiery server
+$username = 'admin'
+
+# set the password to login to the fiery server
+$password = 'Fiery.1'
+
+# websocket object
+$ws = nil
+
+# login to the fiery server
+def login()
+  login_json = {
+    :username => $username,
+    :password => $password,
+    :accessrights => $api_key
+  }
+
+  client = RestClient::Resource.new "https://#{$hostname}/live/api/v2/", :headers => {}, :verify_ssl => OpenSSL::SSL::VERIFY_NONE
+
+  request = login_json.to_json
+  response = client['login'].post request, { :content_type => 'application/json' }
+
+  client.options[:headers][:cookies] = response.cookies
+
+  p ''
+  p 'Login'
+  p response
+  client
+end
+
+# logout from the fiery server
+def logout(client)
+  response = client['logout'].post nil
+
+  client.options[:headers][:cookies] = response.cookies
+
+  p ''
+  p 'Logout'
+  p response
+end
+
+def receive_fiery_events
+  # login to fiery server
+  client = login
+
+  # get the session cookies
+  cookies = client.options[:headers][:cookies]
+
+  # add cookie to the custom header for websocket client
+  custom_headers = {'cookie' => cookies.to_s.delete("\"{>}")}
+
+  # set the websocket server address
+  server_address = "wss://#{$hostname}/live/api/v2/events"
+
+  # establish websocket connection and receive fiery events
+  run_websocket(server_address, custom_headers)
+
+  # logout from fiery server and close websocket connection
+  logout client
+end
+
+def receive_fiery_status_change_events
+
+  puts
+  puts "Scenario: Receive only Fiery status change events"
+  puts "Press <Enter> when you want to run next scenario"
+
+  # ignore all events except device
+  $ws.send(
+  {
+    "jsonrpc" => "2.0",
+    "method" => "ignore",
+    "params" => ["accounting", "job", "jobprogress", "preset", "property", "queue"],
+    "id" => 1
+  }.to_json)
+
+  # receive device events
+  $ws.send(
+  {
+    "jsonrpc" => "2.0",
+    "method" => "receive",
+    "params" => ["device"],
+    "id" => 2
+  }.to_json)
+
+  gets
+end
+
+def receive_job_is_printing_events
+  puts
+  puts "Scenario: Receive only job is printing? events"
+  puts "Press <Enter> when you want to run next scenario"
+
+  # ignore all events except job events
+  $ws.send(
+  {
+    "jsonrpc" => "2.0",
+    "method" => "ignore",
+    "params" => ["accounting", "device", "jobprogress", "preset", "property", "queue"],
+    "id" => 1
+  }.to_json)
+
+  # receive job events
+  $ws.send(
+  {
+    "jsonrpc" => "2.0",
+    "method" => "receive",
+    "params" => ["job"],
+    "id" => 2
+  }.to_json)
+
+  # receive job events only if they contain <is printing?> key in the <attributes>
+  $ws.send(
+  {
+    "jsonrpc" => "2.0",
+    "method"  => "filter",
+    "params" =>
+    {
+      "eventKind" => "job",
+      "mode" => "add",
+      "attr" => { "attributes" => ["is printing?"] }
+    },
+    "id" => 3
+  }.to_json)
+
+  gets
+end
+
+def receive_job_is_printing_events_in_batch_mode
+  puts
+  puts "Scenario: Receive only job is printing? events in batch mode"
+  puts "Press <Enter> when you want to run next scenario"
+
+  $ws.send(
+  [
+    # ignore all events except job events
+    {
+      "jsonrpc" => "2.0",
+      "method" => "ignore",
+      "params" => ["accounting", "device", "jobprogress", "preset", "property", "queue"],
+      "id" => 1
+    },
+
+    # receive job events
+    {
+      "jsonrpc" => "2.0",
+      "method" => "receive",
+      "params" => ["job"],
+      "id" => 2
+    },
+
+    # receive job events only if they contain <is printing?> key in the <attributes>
+    {
+      "jsonrpc" => "2.0",
+      "method"  => "filter",
+      "params" =>
+      {
+        "eventKind" => "job",
+        "mode" => "add",
+        "attr" => { "attributes" => ["is printing?"] }
+      },
+      "id" => 3
+    }
+  ].to_json)
+
+  gets
+end
+
+def run_websocket(server_address, custom_headers)
+  EM.run {
+    $ws = Faye::WebSocket::Client.new(server_address, nil, :headers => custom_headers)
+
+    $ws.on :open do |event|
+      puts "new websocket connection is established"
+      EM.defer(
+        proc do
+          receive_fiery_status_change_events
+          receive_job_is_printing_events
+          receive_job_is_printing_events_in_batch_mode
+          $ws.close
+        end
+      )
+    end
+
+    $ws.on :message do |event|
+      puts event.data.to_json
+    end
+
+    $ws.on :close do |event|
+      p [:close, event.code, event.reason, Time.now]
+      EM::stop_event_loop
+    end
+  }
+end
+
+receive_fiery_events
